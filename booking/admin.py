b7482b051cb639976138b2bdc6334ec5fc871b49
abenjamin5-collab <abenjamin5@live.maryville.edu>
2024-07-21 10:02:31 -0400

    Delete booking directory

--- booking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Coach, Lesson, Review
-
-admin.site.register(User)
-admin.site.register(Coach)
-admin.site.register(Lesson)
-admin.site.register(Review)
-
